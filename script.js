3adbd92e1c45bac46c6cbada92d798fefe5524a5
Shaik Mohammed Naveed <83392132+Mohammednaveed@users.noreply.github.com>
2023-04-11 14:09:04 +0530

    Update Keywords

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // import data from './datascript.mjs';
 // import keywords from './keywords.mjs';
-var keywords = ['hello','located', 'bitm', 'cse', 'the', 'computer', 'science', 'artificial', 'intelligence', 'and', 'college', 'ballari', 'institute', 'of', 'technology', 'management', 'principal', 'director', 'deputy', 'hod', 'faculties', 'faculty', 'experience', 'highest', 'package', 'campus', 'naveed', 'mahika', 'Students', 'student', 'good', 'fees', 'timings', 'bus', 'engineering', 'electrical', 'mechanical', 'electronics', 'data', 'who', 'what', 'when', 'what', 'where', 'which', 'did', 'didn\'t', 'does', 'do', 'don\'t', 'doesn\'t', 'had', 'how', 'much', 'payment', 'every', 'everyone', 'someone', 'somewhere', 'location', 'locality', 'place', 'new', 'existing', 'old', 'established'];
+var keywords = ['hello','hi','he','she','they','them','located', 'bitm', 'cse', 'the', 'computer', 'science', 'artificial', 'intelligence', 'and', 'college', 'ballari', 'institute', 'of', 'technology', 'management', 'principal', 'director', 'deputy', 'hod', 'faculties', 'faculty', 'experience', 'highest', 'package', 'campus', 'naveed', 'mahika', 'Students', 'student', 'good', 'fees', 'timings', 'bus', 'engineering', 'electrical', 'mechanical', 'electronics', 'data', 'who', 'what', 'when', 'what', 'where', 'which', 'did', 'didn\'t', 'does', 'do', 'don\'t', 'doesn\'t', 'had', 'how', 'much', 'payment', 'every', 'everyone', 'someone', 'somewhere', 'location', 'locality', 'place', 'new', 'existing', 'old', 'established'];
 var data = {
   "intents": [
     {
