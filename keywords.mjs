de4e8f2161346a6ba8c5d86cb5a5a938d6f426dc
MohammedNaveed <naveedmohammed687@gmail.com>
2023-09-02 13:22:04 +0530

    seprated data

--- keywords.mjs
+++
@@ -1,3 +0,0 @@
-var keywords = ['hello', 'bitm', 'cse', 'the', 'computer', 'science', 'artificial', 'intelligence', 'and', 'college', 'ballari', 'institute', 'of', 'technology', 'management', 'principal', 'director', 'deputy', 'hod', 'faculties', 'faculty', 'experience', 'highest', 'package', 'campus', 'naveed', 'mahika', 'Students', 'student', 'good', 'fees', 'timings', 'bus', 'engineering', 'electrical', 'mechanical', 'electronics', 'data', 'who', 'what', 'when', 'what', 'where', 'which', 'did', 'didn\'t', 'does', 'do', 'don\'t', 'doesn\'t', 'had', 'how', 'much', 'payment', 'every', 'everyone', 'someone', 'somewhere', 'location', 'locality', 'place', 'new', 'existing', 'old', 'established','AIML','ME','EEE','ECE','Civil'];
-
-export default keywords;
